12ef63400ec691e5515462ad33def125f0218ecd
Daniel Zimmerman <daniel.zimmerman@gmail.com>
2020-06-16 21:39:13 -0600

    Adding an example of a try catch finally

---
+++ trycatchfinally.js
@@ -0,0 +1,11 @@
+// A good simple example of a try, catch, finally
+
+function reverseString(s) {
+  try {
+    s = s.split("").reverse().join("");
+  } catch (e) {
+    console.log(e.message);
+  } finally {
+    console.log(s);
+  }
+}
